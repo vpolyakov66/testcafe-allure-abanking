0a983eb0b9d522f785877630f73d769fdb09b977
vpoliakov <vpoliakov@nopaper.ru>
2025-01-21 15:14:15 +0500

    fix tslib error

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,18 @@
 import typescript from 'rollup-plugin-typescript2';
+import path from 'path';
 
 export default ['src/index.ts', 'src/utils.ts'].map((input) => ({
   input,
   plugins: [
     typescript({
       tsconfig: 'tsconfig.prod.json',
+      tslib: path.resolve(__dirname, 'node_modules/tslib'), // Указываем путь к tslib
     }),
   ],
   external: ['allure-js-commons', 'rimraf', 'merge-anything', 'path', 'uuid', 'fs', 'strip-ansi'],
   output: {
-    exports: 'auto',
+    exports: 'auto', // Сохраняем auto, если экспорт должен быть автоматически определен
     dir: 'dist',
-    format: 'cjs',
+    format: 'cjs',  // Можно сменить на 'esm', если нужно
   },
 }));
